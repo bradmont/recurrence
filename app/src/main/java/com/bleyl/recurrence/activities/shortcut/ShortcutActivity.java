b0f62c81732799ca9c0a3b67352a5e6db46bb577
Brad Stewart <brad@bradstewart.ca>
2026-09-05 21:53:21 -0700

    Step 14: Fix ShortcutActivity - use ShortcutManagerCompat

--- app/src/main/java/com/bleyl/recurrence/activities/shortcut/ShortcutActivity.java
+++ app/src/main/java/com/bleyl/recurrence/activities/shortcut/ShortcutActivity.java
@@ -1,23 +1,33 @@
 package com.bleyl.recurrence.activities.shortcut;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.pm.ShortcutInfoCompat;
+import androidx.core.content.pm.ShortcutManagerCompat;
+import androidx.core.graphics.drawable.IconCompat;
+
 import com.bleyl.recurrence.R;
 
-public class ShortcutActivity extends Activity {
+public class ShortcutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent();
-        Intent shortcutActivity = new Intent(this, CreateEditShortcut.class);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutActivity);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.add_reminder));
-        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, Intent.ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher));
-        setResult(RESULT_OK, intent);
+        if (ShortcutManagerCompat.isRequestPinShortcutSupported(this)) {
+            Intent launchIntent = new Intent(this, CreateEditShortcut.class);
+            launchIntent.setAction(Intent.ACTION_VIEW);
+
+            ShortcutInfoCompat shortcutInfo = new ShortcutInfoCompat.Builder(this, "add_reminder")
+                .setShortLabel(getString(R.string.add_reminder))
+                .setIcon(IconCompat.createWithResource(this, R.mipmap.ic_launcher))
+                .setIntent(launchIntent)
+                .build();
+
+            ShortcutManagerCompat.requestPinShortcut(this, shortcutInfo, null);
+        }
 
         finish();
     }
